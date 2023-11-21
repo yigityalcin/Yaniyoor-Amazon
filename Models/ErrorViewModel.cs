57c1f5ea579cf4987b64bb8395a35befb8d09628
Yigit Yalcin <yigityalciin@hotmail.com>
2023-11-21 05:23:13 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Yaniyor_Amazon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
